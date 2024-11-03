1994b48d6019f6cf473670300815ff78c104b6fc
Andrey Zashchepin <tishina-az@mail.ru>
2024-11-03 21:33:58 +0500

    add stocks function

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,14 @@
 import json
+import os
 from datetime import datetime
 
 import pandas as pd
 import requests
+from dotenv import load_dotenv
 from pandas import DataFrame
+
+load_dotenv()
+api_stock = os.getenv("API_KEY_STOCK")
 
 
 def hello_user() -> str:
@@ -44,8 +49,10 @@ def get_card_info(df: DataFrame, date_start: datetime, date_end: datetime) -> li
     включающими сумму расходов и кэшбека в указанный период.
     """
 
-    df_by_date = df[(pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S")
-                     .between(date_start, date_end)) & (df["Сумма платежа"] < 0)]
+    df_by_date = df[
+        (pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S").between(date_start, date_end))
+        & (df["Сумма платежа"] < 0)
+    ]
     df_by_card = (
         df_by_date[df_by_date["Номер карты"].notna()]
         .groupby("Номер карты")
@@ -69,9 +76,11 @@ def get_card_info(df: DataFrame, date_start: datetime, date_end: datetime) -> li
 def get_top_transactions(df: DataFrame, date_start: datetime, date_end: datetime) -> list[dict]:
     """ """
 
-    df_by_date = df[(pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S")
-                     .between(date_start, date_end)) & (df["Сумма платежа"] < 0)]
-    df_by_values = df_by_date.sort_values(by='Сумма операции с округлением', ascending=False)
+    df_by_date = df[
+        (pd.to_datetime(df["Дата операции"], format="%d.%m.%Y %H:%M:%S").between(date_start, date_end))
+        & (df["Сумма платежа"] < 0)
+    ]
+    df_by_values = df_by_date.sort_values(by="Сумма операции с округлением", ascending=False)
 
     top_transactions = []
     for index, row in df_by_values.iterrows():
@@ -82,44 +91,46 @@ def get_top_transactions(df: DataFrame, date_start: datetime, date_end: datetime
                 "date": row["Дата операции"][:10],
                 "amount": row["Сумма операции с округлением"],
                 "category": row["Категория"],
-                "description": row["Описание"]
+                "description": row["Описание"],
             }
             top_transactions.append(transactions_dict)
 
     return top_transactions
 
 
-def current_exchange_rate(path_to_json: str):
-    """ """
+def current_exchange_rate(path_to_json: str) -> list[dict]:
+    """Функция принимает путь до файла со списком валют и возвращает список с текущим курсом"""
 
     with open(path_to_json) as f:
         data_cur = json.load(f)
     currency_list = data_cur.get("user_currencies")
 
     currency_rates = []
-    response = requests.get(f'https://www.cbr-xml-daily.ru/daily_json.js')
+    response = requests.get("https://www.cbr-xml-daily.ru/daily_json.js")
     if response.status_code != 200:
-        raise ValueError(f"Failed to get currency rate")
+        raise ValueError("Ошибка при получении текущего курса валют!")
     data = response.json()
     for currency in currency_list:
         currency_data = data["Valute"].get(currency)
-        currency_rates.append({
-            "currency": currency,
-            "rate": round(currency_data["Value"], 2 )
-        })
+        currency_rates.append({"currency": currency, "rate": round(currency_data["Value"], 2)})
         if not currency_data:
-            raise ValueError(f"No data for currency {currency}")
+            raise ValueError(f"Не найдены данные для валюты: {currency}!")
 
     return currency_rates
 
 
-if __name__ == "__main__":
-    path = "../data/operations.xlsx"
-    path_json = "../user_settings.json"
-    transactions = read_xlsx(path)
-    date_1, date_2 = get_range_of_date("2020-03-2 12:00:00")
-    print(date_1, date_2)
-    print((hello_user()))
-    print(get_card_info(transactions, date_1, date_2))
-    print(get_top_transactions(transactions, date_1, date_2))
-    print(current_exchange_rate(path_json))
+def current_values_stocks(path_to_json: str) -> list[dict]:
+    """Функция принимает путь к списку тикеров акция и возвращает списко словарей со стоимостью"""
+
+    with open(path_to_json) as f:
+        data_st = json.load(f)
+    stocks_list = data_st.get("user_stocks")
+
+    stock_prices = []
+    for stock in stocks_list:
+        url = f"https://financialmodelingprep.com/api/v3/quote/{stock}?apikey={api_stock}"
+        response = requests.get(url)
+        data = response.json()
+        stock_prices.append({"stock": stock, "price": data[0]["price"]})
+
+    return stock_prices
